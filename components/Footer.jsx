f51a16f906c0a9cc76324c45a07d8d2f130f6f61
abhishek hiwarkar <abhiwarkar123@gmail.com>
2024-04-27 13:03:57 +0530

    First Commit
    real estate website

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign='center' p='5' color='gray.600'>
+        Made with ❤️ by Abhishek Hiwarkar
+        <br />
+        &copy; 2024 &mdash; UrbanPulse.in
+    </Box>
+);
+
+export default Footer;
